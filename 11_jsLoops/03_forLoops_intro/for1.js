6307648785222a901b37831fea244316f7c300e8
lopezlbj <clopez@Carloss-MacBook-Pro.local>
2020-01-16 07:07:52 -0600

    js probs

---
+++ 11_jsLoops/03_forLoops_intro/for1.js
@@ -0,0 +1,14 @@
+// Excercise 1
+// for (var i = 0; i < 16; i+=8) {
+//     console.log(i);
+// }
+
+
+Exercise 2
+var str = "ahceclw1xo";
+
+for (var i = 1; i < str.length; i+=2) {
+    console.log(str[i]);
+}
+
+
