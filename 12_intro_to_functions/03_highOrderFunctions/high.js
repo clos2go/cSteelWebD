f9ba8e0bc146be1502dcb9384e87565fd842b76b
lopezlbj <clopez@carloss-mbp.lan>
2020-01-16 20:35:43 -0600

    higher functions

---
+++ 12_intro_to_functions/03_highOrderFunctions/high.js
@@ -0,0 +1,23 @@
+function sing() {
+    console.log("Twinkle Twinkle little Star.");
+    console.log("How I wonder ...");
+}
+
+sing();
+
+// setInterval(sing, 1000);
+// clearInterval(2);
+var timesRun = 0;
+var interval = setInterval(function() {
+    timesRun += 1;
+    if (timesRun === 3) {
+        clearInterval(interval)
+    }
+    console.log("I am an anonymous function!");
+    console.log("This is awesome!!");
+}, 2000);
+
+
+
+    
+    
