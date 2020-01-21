fcf9908c2d4165f7dd1993783f6cece456c12f41
lopezlbj <clopez@carloss-mbp.lbj.austinisd.org>
2020-01-21 11:43:52 -0600

    scorekeeper 2 completed.

--- 16_advancedDomManipulations/03_scoreKeeperProject/score.js
+++ 16_advancedDomManipulations/03_scoreKeeperProject/score.js
@@ -12,11 +12,13 @@ var p2Score = 0;
 var winningScore = 7;
 var gameOver = false;
 
-//SELECT BUTTONS
+//SELECTORS
 var btnP1 = document.getElementById("btnP1");
 var btnP2 = document.getElementById("btnP2");
+var resetBtn = document.getElementById("reset");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
+var winningScoreDisplay = document.querySelector("p span");
 var input = document.querySelector("input");
 var log = document.getElementById("values");
 var h3Display = document.getElementsByTagName("h3")[0];
@@ -27,9 +29,13 @@ btnP1.addEventListener("click", function () {
     if (!gameOver) {
         p1Score++;
         if (p1Score === winningScore) {
+            p1Display.classList.add("winner");
+            h3Display.classList.add("winner");
             gameOver = true;
-            p1Display.style.color = "green";
-            h3Display.style.color = "green";
+
+            //Change style without changing class
+            // p1Display.style.color = "green";
+            // h3Display.style.color = "green";
             h3Display.textContent = "Player 1 is the winner!!! Congrats!!!"
             "Player 1 is the winner!!! Congrats!!! \n\n Click Reset to play again. "
         }
@@ -41,31 +47,55 @@ btnP2.addEventListener("click", function () {
     if (!gameOver) {
         p2Score++;
         if (p2Score === winningScore) {
+            p2Display.classList.add("winner");
+            h3Display.classList.add("winner");
             gameOver = true;
-            p2Display.style.color = "green";
-            h3Display.style.color = "green";
+            // p2Display.style.color = "green";
+            // h3Display.style.color = "green";
             h3Display.textContent = "Player 2 is the winner1!! Congrats!!! \n \n Click reset to play again!!"
         }
         p2Display.textContent = p2Score;
     }
+});
+
+//resets screen
+resetBtn.addEventListener("click", function () {
+    reset();
 })
-reset.addEventListener("click", function () {
+
+function reset() {
     // alert("reset clicked");
-    gameOver = false;
+
     p1Score = 0;
     p2Score = 0;
-    h3Display.style.color = "black";
-    p1Display.style.color = "black";
-    p2Display.style.color = "black";
+    //with classlist
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+    h3Display.classList.remove("winner");
+    //whithout changing class
+    // h3Display.style.color = "black";
+    // p1Display.style.color = "black";
+    // p2Display.style.color = "black";
     h3Display.textContent = "";
     p1Display.textContent = p1Score;
     p2Display.textContent = p2Score;
     input.value = "";
+    winningScoreDisplay.textContent = 7;
+    gameOver = false;
     // console.log(p1Score, p2Score);
-})
+}
+
+
 
 //Logging input value to <p></p>
-input.addEventListener("input", function (e) {
-    log.textContent = e.target.value;
-    winningScore = parseInt(log.textContent);
+// input.addEventListener("input", function (e) {
+//     log.textContent = e.target.value;
+//     winningScore = parseInt(log.textContent);
+// });
+
+input.addEventListener("change", function () {
+    var newScore = Number(this.value);
+    winningScoreDisplay.textContent = newScore;
+    winningScore = newScore;
+    // reset();
 });
