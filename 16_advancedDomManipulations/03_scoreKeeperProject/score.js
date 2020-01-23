1ac8d4f54f32e44305a2666be28012805b70acce
lopezlbj <clopez@carloss-mbp.lbj.austinisd.org>
2020-01-23 05:43:42 -0600

    color game continye

--- 16_advancedDomManipulations/03_scoreKeeperProject/score.js
+++ 16_advancedDomManipulations/03_scoreKeeperProject/score.js
@@ -1,11 +1,10 @@
-//set starting score 
+//set starting score
 //create player counters
 //player 1 score on click
 //player 2  score on click
 //reset score on click
 //player 1 input to render <p></p>
-//render player score after each click 
-
+//render player score after each click
 
 var p1Score = 0;
 var p2Score = 0;
@@ -13,79 +12,76 @@ var winningScore = 7;
 var gameOver = false;
 
 //SELECTORS
-var btnP1 = document.getElementById("btnP1");
-var btnP2 = document.getElementById("btnP2");
-var resetBtn = document.getElementById("reset");
-var p1Display = document.querySelector("#p1Display");
-var p2Display = document.querySelector("#p2Display");
-var winningScoreDisplay = document.querySelector("p span");
-var input = document.querySelector("input");
-var log = document.getElementById("values");
-var h3Display = document.getElementsByTagName("h3")[0];
-
+var btnP1 = document.getElementById('btnP1');
+var btnP2 = document.getElementById('btnP2');
+var resetBtn = document.getElementById('reset');
+var p1Display = document.querySelector('#p1Display');
+var p2Display = document.querySelector('#p2Display');
+var winningScoreDisplay = document.querySelector('p span');
+var input = document.querySelector('input');
+var log = document.getElementById('values');
+var h3Display = document.getElementsByTagName('h3')[0];
 
 //Score event listeners
-btnP1.addEventListener("click", function () {
-    if (!gameOver) {
-        p1Score++;
-        if (p1Score === winningScore) {
-            p1Display.classList.add("winner");
-            h3Display.classList.add("winner");
-            gameOver = true;
+btnP1.addEventListener('click', function() {
+	if (!gameOver) {
+		p1Score++;
+		if (p1Score === winningScore) {
+			p1Display.classList.add('winner');
+			h3Display.classList.add('winner');
+			gameOver = true;
 
-            //Change style without changing class
-            // p1Display.style.color = "green";
-            // h3Display.style.color = "green";
-            h3Display.textContent = "Player 1 is the winner!!! Congrats!!!"
-            "Player 1 is the winner!!! Congrats!!! \n\n Click Reset to play again. "
-        }
-        p1Display.textContent = p1Score;
-    }
-})
+			//Change style without changing class
+			// p1Display.style.color = "green";
+			// h3Display.style.color = "green";
+			h3Display.textContent = 'Player 1 is the winner!!! Congrats!!!';
+			('Player 1 is the winner!!! Congrats!!! \n\n Click Reset to play again. ');
+		}
+		p1Display.textContent = p1Score;
+	}
+});
 
-btnP2.addEventListener("click", function () {
-    if (!gameOver) {
-        p2Score++;
-        if (p2Score === winningScore) {
-            p2Display.classList.add("winner");
-            h3Display.classList.add("winner");
-            gameOver = true;
-            // p2Display.style.color = "green";
-            // h3Display.style.color = "green";
-            h3Display.textContent = "Player 2 is the winner1!! Congrats!!! \n \n Click reset to play again!!"
-        }
-        p2Display.textContent = p2Score;
-    }
+btnP2.addEventListener('click', function() {
+	if (!gameOver) {
+		p2Score++;
+		if (p2Score === winningScore) {
+			p2Display.classList.add('winner');
+			h3Display.classList.add('winner');
+			gameOver = true;
+			// p2Display.style.color = "green";
+			// h3Display.style.color = "green";
+			h3Display.textContent = 'Player 2 is the winner1!! Congrats!!! \n \n Click reset to play again!!';
+		}
+		p2Display.textContent = p2Score;
+	}
 });
 
 //resets screen
-resetBtn.addEventListener("click", function () {
-    reset();
-})
+resetBtn.addEventListener('click', function() {
+	reset();
+});
 
 function reset() {
-    // alert("reset clicked");
+	// alert("reset clicked");
 
-    p1Score = 0;
-    p2Score = 0;
-    //with classlist
-    p1Display.classList.remove("winner");
-    p2Display.classList.remove("winner");
-    h3Display.classList.remove("winner");
-    //whithout changing class
-    // h3Display.style.color = "black";
-    // p1Display.style.color = "black";
-    // p2Display.style.color = "black";
-    h3Display.textContent = "";
-    p1Display.textContent = p1Score;
-    p2Display.textContent = p2Score;
-    input.value = "";
-    winningScoreDisplay.textContent = 7;
-    gameOver = false;
-    // console.log(p1Score, p2Score);
+	p1Score = 0;
+	p2Score = 0;
+	//with classlist
+	p1Display.classList.remove('winner');
+	p2Display.classList.remove('winner');
+	h3Display.classList.remove('winner');
+	//whithout changing class
+	// h3Display.style.color = "black";
+	// p1Display.style.color = "black";
+	// p2Display.style.color = "black";
+	h3Display.textContent = '';
+	p1Display.textContent = p1Score;
+	p2Display.textContent = p2Score;
+	input.value = '';
+	winningScoreDisplay.textContent = 7;
+	gameOver = false;
+	// console.log(p1Score, p2Score);
 }
-
-
 
 //Logging input value to <p></p>
 // input.addEventListener("input", function (e) {
@@ -93,9 +89,9 @@ function reset() {
 //     winningScore = parseInt(log.textContent);
 // });
 
-input.addEventListener("change", function () {
-    var newScore = Number(this.value);
-    winningScoreDisplay.textContent = newScore;
-    winningScore = newScore;
-    // reset();
+input.addEventListener('change', function() {
+	var newScore = Number(this.value);
+	winningScoreDisplay.textContent = newScore;
+	winningScore = newScore;
+	// reset();
 });
--- 16_advancedDomManipulations/07_colorEventsProject/color.js
+++ 16_advancedDomManipulations/07_colorEventsProject/color.js
@@ -1,47 +1,103 @@
 //Variables
 var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-]
-var pickedColor = colors[3];
+	'rgb(255, 0, 0)',
+	'rgb(255, 255, 0)',
+	'rgb(0, 255, 0)',
+	'rgb(0, 255, 255)',
+	'rgb(0, 0, 255)',
+	'rgb(255, 0, 255)'
+];
 
+var pickedColor = pickColor();
+var colors2 = [ pickedColor ];
+var randoColors = [];
+console.log(pickedColor);
+console.log(colors2);
 
 //Selectors
-var squares = document.querySelectorAll(".square");
-var colorDisplay = document.getElementById("colorDisplay");
+var squares = document.querySelectorAll('.square');
+var colorDisplay = document.getElementById('colorDisplay');
+var body = document.querySelector('body');
+var messageDisplay = document.getElementById('message');
+var title = document.querySelector('.title');
+var newColor = document.querySelector('.navbar div');
 
-
-//Stuff
+//Setup Game
 //Adds picked color to color display
-colorDisplay.textContent = pickedColor;
+colorDisplay.innerHTML = '<br>' + pickedColor + '<br>';
 
-//Adds color to each square
+//Adds color to each square then allows you to remove  wrong squares by clicking and tells you whether your click was correct or not
 for (var i = 0; i < squares.length; i++) {
-    //add initial color to squares
-    squares[i].style.backgroundColor = colors[i];
+	//add initial color to squares
+	squares[i].style.backgroundColor = colors[i];
 
-    //add click listeners to squares 
-    squares[i].addEventListener("click", function () {
-        //grab color of clicked
-        var clickedColor = this.style.backgroundColor;
-        //compare  clicked color to pickedColor
-        if (clickedColor == pickedColor) {
-            console.log("yes");
-            for (var j = 0; j < squares.length; j++) {
-                squares[j].style.backgroundColor = pickedColor;
-            }
-        } else {
-            console.log("no")
-            this.style.backgroundColor = "#232323"
-        }
-    });
+	//add click listeners to squares
+	squares[i].addEventListener('click', function() {
+		//grab color of clicked
+		var clickedColor = this.style.backgroundColor;
+		//compare  clicked color to pickedColor
+		if (clickedColor == pickedColor) {
+			title.style.backgroundColor = pickedColor;
+			// for (var j = 0; j < squares.length; j++) {
+			//     squares[j].style.backgroundColor = pickedColor;
+			changeColors(pickedColor);
+			messageDisplay.textContent = 'Correctamundo!!!';
+		} else {
+			this.style.backgroundColor = '#232323';
+			messageDisplay.textContent = 'Try Again';
+		}
+	});
 }
 
+//eventListener for new colors button
+// newColor.addEventListener('click', function () {
 
+// });
 
-// //Adds pickedColor to h1
-// colorDisplay.innerHTML = "<br>" + pickedColor + "<br>";
+//Functions
+
+//Changes all squares to same color
+function changeColors(color) {
+	//loop through all the colors and change colors
+	for (var i = 0; i < squares.length; i++) {
+		//Change color
+		squares[i].style.background = color;
+		console.log(colors[i]);
+	}
+}
+
+// Create array with pickedColor and 5 more random colors and then randomize array order.
+function newColors() {
+	//creates new color array
+	for (var i = 1; i < squares.length; i++) {
+		colors2.push(pickColor());
+	}
+	console.log(colors2);
+	colors2.sort();
+	colors2.reverse();
+	console.log(colors2);
+	for (var i = 0; i < squares.length; i++) {
+		//add initial color to squares
+		squares[i].style.backgroundColor = colors2[i];
+	}
+}
+console.log(newColors());
+
+function pickColor() {
+	return 'rgb(' + rgbNums() + ', ' + rgbNums() + ', ' + rgbNums() + ')';
+}
+
+function rgbNums() {
+	var randomNum = Math.floor(Math.random() * 256);
+	return randomNum;
+}
+
+function reset() {
+	pickColor();
+	pickedColor = pickColor();
+	colors2 = pickedColor;
+	console.log(colors2);
+	console.log(pickedColor);
+	colorDisplay.innerHTML = '<br>' + pickedColor + '<br>';
+	newColors();
+}
