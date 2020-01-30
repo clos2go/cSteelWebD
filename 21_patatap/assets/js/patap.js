049b49fbec716331d4a4308b21c4cb98f8217462
lopezlbj <clopez@carloss-mbp.lbj.austinisd.org>
2020-01-30 07:03:07 -0600

    patap round2

--- 21_patatap/assets/js/patap.js
+++ 21_patatap/assets/js/patap.js
@@ -1,13 +1,38 @@
-var text = new PointText({
-	point: view.center,
-	content: 'Click here to focus and then press some keys.',
-	justification: 'center',
-	fontSize: 15
+//display which key is pressed
+// var text = new PointText({
+// 	point: view.center,
+// 	content: 'Click here to focus and then press some keys.',
+// 	justification: 'center',
+// 	fontSize: 15
+// });
+
+var sound = new Howl({
+	src: 'sounds/bubbles.mp3',
+	html5: true,
+	format: ['mp3', 'aac']
 });
 
+var circles = [];
+
 function onKeyDown(event) {
-	// When a key is pressed, set the content of the text item:
-	text.content = 'The ' + event.key + ' key was pressed!';
-	new Path.Circle(new Point(100, 100), 10).fillColor = "orange";
-	console.log(view.size)
+	sound.play();
+	var maxPoint = new Point(view.size.width, view.size.height);
+	var randomPoint = Point.random();
+	// A point between {x:0, y:0,} and {x:100, y:100}
+	var point = maxPoint * randomPoint;
+	// // When a key is pressed, set the content of the text item:
+	// text.content = 'The ' + event.key + ' key was pressed!';
+
+	var newCircle = new Path.Circle(point, 100);
+	newCircle.fillColor = "orange";
+	circles.push(newCircle);
+}
+
+
+
+function onFrame(event) {
+	for (var i = 0; i < circles.length; i++) {
+		circles[i].fillColor.hue += 1;
+		circles[i].scale(.9);
+	}
 }
