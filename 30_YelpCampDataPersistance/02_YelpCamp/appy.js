979e884ec1bc919a468280f0e592a9c6b4f893e5
lopezlbj <clopez@carloss-mbp.lbj.austinisd.org>
2020-02-06 15:47:13 -0600

    adding db to yelp app

--- 30_YelpCampDataPersistance/02_YelpCamp/appy.js
+++ 30_YelpCampDataPersistance/02_YelpCamp/appy.js
@@ -1,56 +1,74 @@
-var express = require("express");
-var app = express();
-var request = require("request");
-var bodyParser = require("body-parser");
-port = 3000;
+var express = require('express'),
+	app = express(),
+	request = require('request'),
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose'),
+	port = 3000;
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(express.static("public"));
-app.set("view engine", "ejs");
+mongoose.connect('mongodb://localhost/yelp_camp');
 
-var campgrounds = [{
-        name: "Lazy Creek",
-        image: "https://pixabay.com/get/52e7d0454d55a814f6da8c7dda793f7f1636dfe2564c704c7d2f73d7924dc25c_340.jpg"
-    },
-    {
-        name: "Ruidoso",
-        image: "https://pixabay.com/get/57e1d14a4e52ae14f6da8c7dda793f7f1636dfe2564c704c7d2f73d7924dc25c_340.jpg"
-    },
-    {
-        name: "Cloudcroft",
-        image: "https://pixabay.com/get/55e8dc404f5aab14f6da8c7dda793f7f1636dfe2564c704c7d2f73d7924dc25c_340.jpg"
-    }
-];
+app.use(
+	bodyParser.urlencoded({
+		extended : true
+	})
+);
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
-
-app.get("/", function (req, res) {
-    res.render("landing");
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+	name  : String,
+	image : String
 });
 
-app.get("/campgrounds", function (req, res) {
-    res.render("campgrounds", {
-        campgrounds: campgrounds,
-    });
+var Campground = mongoose.model('Campground', campgroundSchema);
 
+Campground.create(
+	{
+		name  : 'Ruidoso',
+		image : 'https://pixabay.com/get/57e1d14a4e52ae14f6da8c7dda793f7f1636dfe2564c704c7d2f73d7924dc25c_340.jpg'
+	},
+	function(err, campground) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(campground);
+		}
+	}
+);
+
+app.get('/', function(req, res) {
+	res.render('landing');
 });
 
-app.post("/campgrounds", function (req, res) {
-    var name = req.body.name;
-    var image = req.body.image;
-    var newCampground = {
-        name: name,
-        image: image
-    };
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
-    // res.send("YOU made it POSt!")
+app.get('/campgrounds', function(req, res) {
+	//Get all campgrounds from DB
+	Campground.find({}, function(err, allCampgrounds) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render('campgrounds', { campgrounds: allCampgrounds });
+		}
+	});
+	// res.render('campgrounds', {
+	// 	campgrounds : campgrounds
+	// });
 });
 
-app.get("/campgrounds/new", function (req, res) {
-    res.render("new");
+app.post('/campgrounds', function(req, res) {
+	var name = req.body.name;
+	var image = req.body.image;
+	var newCampground = {
+		name  : name,
+		image : image
+	};
+	campgrounds.push(newCampground);
+	res.redirect('/campgrounds');
+	// res.send("YOU made it POSt!")
 });
 
+app.get('/campgrounds/new', function(req, res) {
+	res.render('new');
+});
 
 app.listen(port, () => console.log(`Yelp app is listening on port ${3000}!`));
